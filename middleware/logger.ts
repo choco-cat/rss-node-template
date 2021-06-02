848f782af0f9e8e5707bcd5f7f60b5914ca50bc8
Panfilenka Natallia <choco-cat@mail.ru>
2021-06-02 11:21:50 +0300

    feat: use winston for logs

--- middleware/logger.ts
+++ middleware/logger.ts
@@ -1,5 +1,6 @@
 const morgan = require('morgan');
 const { createWriteStream } = require('fs');
+const winston = require('./winston.ts');
 
-const morganDev = morgan('dev', {stream: createWriteStream('access.log')});
+const morganDev = morgan('dev', {stream: winston.stream});
 module.exports = morganDev;
---
+++ middleware/winston.ts
@@ -0,0 +1,36 @@
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+    level: 'silly',
+    format: format.combine(
+        format.colorize(),
+        format.cli(),
+    ),
+    transports: [
+        new transports.Console(),
+        new transports.File({
+            filename: 'error.log',
+            level: 'error',
+            format: format.combine(
+                format.uncolorize(),
+                format.json()
+            )
+        }),
+        new transports.File({
+            filename: 'info.log',
+            level: 'info',
+            format: format.combine(
+                format.uncolorize(),
+                format.json()
+            )
+        })
+    ]
+});
+
+logger.stream = {
+    write(message:string){
+        logger.info(message);
+    }
+};
+
+module.exports = logger;
