9bc9319e2295ab9cc612dfc65611c03352814c47
Panfilenka Natallia <choco-cat@mail.ru>
2021-06-04 11:07:10 +0300

    feat: errors processing for tasks

--- src/resources/tasks/task.memory.repository.ts
+++ src/resources/tasks/task.memory.repository.ts
@@ -9,7 +9,7 @@ let Tasks: ITask[] = [];
  * @param {string} boardId id of board from which to select task
  * @returns {Promise<Array.<Task>>} array of tasks objects
  */
-const getAll = async (boardId: string): Promise<ITask[]> => Tasks.filter((el) => el.boardId === boardId)
+const getAll = async (boardId: string): Promise<ITask[]> => Array.isArray(Tasks) ? Tasks.filter((el) => el.boardId === boardId): new ValidationError('Server error', '500');
 /**
  * Adds a new task object to array of tasks objects, returns new task
  *
@@ -17,6 +17,9 @@ const getAll = async (boardId: string): Promise<ITask[]> => Tasks.filter((el) =>
  * @returns {Promise<Task>} task new object
  */
 const addTask = async (taskRow: ITask): Promise<ITask> => {
+  if (!Array.isArray(Tasks)) {
+    return new ValidationError('Server error', '500');
+  }
   Tasks.push(taskRow);
   return taskRow;
 }
@@ -30,7 +33,7 @@ const addTask = async (taskRow: ITask): Promise<ITask> => {
 const getTask = async (boardId: string, taskId: string): Promise<ITask> => {
   const task = Tasks.find((el) =>  el.id === taskId && el.boardId === boardId) || null;
   if (!task) {
-    return new ValidationError('Error get task!', '404');
+    return new ValidationError(`Task with id = ${taskId} not found`, '404');
   }
   return task;
 }
@@ -41,6 +44,9 @@ const getTask = async (boardId: string, taskId: string): Promise<ITask> => {
  * @returns {Promise<Array<Task>>} An updated array of tasks objects
  */
 const deleteTasksFromUser = async (userId: string): Promise<ITask[]> => {
+  if (!Array.isArray(Tasks)) {
+    return new ValidationError('Server error', '500');
+  }
   Tasks = Tasks.map((el) =>  el.userId === userId ? { ...el, userId: null } : el);
   return Tasks;
 }
@@ -51,7 +57,10 @@ const deleteTasksFromUser = async (userId: string): Promise<ITask[]> => {
  * @returns {Promise<Task>} updated task
  */
 const updateTask = async (taskRow: ITask): Promise<ITask> => {
-  const task = Tasks.find((el) =>  el.id === taskRow.id && el.boardId === taskRow.boardId);
+  const task = Tasks.find((el) =>  el.id === taskRow.id && el.boardId === taskRow.boardId) || null;
+  if (!task) {
+    return new ValidationError(`Task with id = ${taskRow.id} not found`, '404');
+  }
   if (task !== undefined) {
     task.title = taskRow.title;
     task.order = taskRow.order;
@@ -69,6 +78,9 @@ const updateTask = async (taskRow: ITask): Promise<ITask> => {
  */
 const deleteTask = async (taskId: string): Promise<boolean> => {
   const task = Tasks.find((el) =>  el.id === taskId);
+  if (!task) {
+    return new ValidationError(`Task with id = ${taskId} not found`, '404');
+  }
   const index = Tasks.indexOf(task);
   if (index > -1) {
     Tasks.splice(index, 1);
